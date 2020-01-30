37bcf42ecf77b9e2d06c3f25fe6c24dffe51ee13
Joseph Wangemann <joseph@>
2020-01-30 09:44:02 -0800

    add hide/show function to submit button, create constructor function for players, create two players with submit

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
+//business logic
+function Player(name, mark) {
+    this.name = name;
+    this.mark = mark;
+}
+
+
+//user interface
+var xPlayer = "";
+var oPlayer = "";
 $(document).ready(function() {
     $("form").submit(function() {
-
+        event.preventDefault();
+        xPlayer = new Player(($("#xplayer-name").val()), ("X"));
+        oPlayer = new Player(($("#oplayer-name").val()), ("O"));
+        $("#start-screen").hide();
+        $("#play-screen").fadeIn();
     });
 });
