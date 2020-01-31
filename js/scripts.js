78b1c1c4a87b3cee59e86af6aabf1b1273f4ec9b
Joseph Wangemann <joseph@>
2020-01-30 16:22:24 -0800

    add constructor for board which keeps track of turn count with prototype

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,25 @@ function Player(name, mark) {
     this.mark = mark;
 }
 
+function Board() {
+    this.turn = 0;
+}
+
+Board.prototype.updateTurn = function() {
+    this.turn++;
+}
+
 //user interface
 var xPlayer = "";
 var oPlayer = "";
-var turn = 0;
+var currentGame = ""
 $(document).ready(function() {
     //button click to start game
     $("form").submit(function() {
         event.preventDefault();
         xPlayer = new Player(($("#xplayer-name").val()), ("X"));
         oPlayer = new Player(($("#oplayer-name").val()), ("O"));
+        currentGame = new Board();
         $("#xname").text(xPlayer.name);
         $("#oname").text(oPlayer.name);
         $("#start-screen").hide();
@@ -22,17 +31,17 @@ $(document).ready(function() {
     //button click to add X or O to board
     $("#game-board").on("click", "td", function() {
             if (($(this).html()) == "") {
-                if (turn % 2 === 0) {
+                if (currentGame.turn % 2 === 0) {
                 $(this).text(xPlayer.mark);
-                turn++;
+                currentGame.updateTurn();
                 } else {
                 $(this).text(oPlayer.mark);
-                turn++;
+                currentGame.updateTurn();
                 }
             }
             if (($("#a").html()) !== "" && ($("#b").html()) !== "" && ($("#a").html()) == ($("#b").html()) && ($("#b").html()) == ($("#c").html()) || ($("#d").html()) !== "" && ($("#e").html()) !== "" && ($("#d").html()) == ($("#e").html()) && ($("#e").html()) == ($("#f").html()) || ($("#g").html()) !== "" && ($("#h").html()) !== "" && ($("#g").html()) == ($("#h").html()) && ($("#h").html()) == ($("#i").html()) || ($("#a").html()) !== "" && ($("#d").html()) !== "" && ($("#a").html()) == ($("#d").html()) && ($("#d").html()) == ($("#g").html()) || ($("#b").html()) !== "" && ($("#e").html()) !== "" && ($("#b").html()) == ($("#e").html()) && ($("#e").html()) == ($("#h").html()) || ($("#c").html()) !== "" && ($("#f").html()) !== "" && ($("#c").html()) == ($("#f").html()) && ($("#f").html()) == ($("#i").html()) || ($("#a").html()) !== "" && ($("#e").html()) !== "" && ($("#a").html()) == ($("#e").html()) && ($("#e").html()) == ($("#i").html()) || ($("#c").html()) !== "" && ($("#e").html()) !== "" && ($("#c").html()) == ($("#e").html()) && ($("#e").html()) == ($("#g").html())) {
                 var winner = ""
-                if (turn % 2 === 0) {
+                if (currentGame.turn % 2 === 0) {
                     winner = oPlayer.name;
                 } else {
                     winner = xPlayer.name;
@@ -40,7 +49,7 @@ $(document).ready(function() {
                 $("#win-screen").text("And the winner is... " + winner + "!");
                 $("#win-screen").addClass("winner");
                 $("td").addClass("nonclick");
-            } else if (turn == 9) {
+            } else if (currentGame.turn == 9) {
                 $("#win-screen").text("TIE GAME!");
                 $("#win-screen").addClass("tiegame");
             }
